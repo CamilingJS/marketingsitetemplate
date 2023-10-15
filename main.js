64c9c958310a193befe379768568b6a9686ff367
CamilingJS <jonathancamiling@gmail.com>
2023-10-14 17:17:26 -0700

    added click event to close nav bar off click

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.mobile-nav');
+const navBar = document.querySelector('.nav-container')
+
 
 
 navToggle.addEventListener('click', () => {
+    console.log("clicked on icon")
     nav.classList.toggle('nav--visible');
 })
+
+document.onclick = function(e){
+    if(!navBar.contains(e.target)){
+        console.log("clicked outside of icon")
+        nav.classList.remove('nav--visible');
+    }
+}
